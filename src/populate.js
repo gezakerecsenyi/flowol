c671aaac812739e6d6ce21624e4d2a0fd3f2deed
gezakerecsenyi <32415108+gezakerecsenyi@users.noreply.github.com>
2019-05-12 20:05:01 +0100

    Add icons

--- src/populate.js
+++ src/populate.js
@@ -7,7 +7,7 @@ let elements = document.createElement("div");
 
 function radioStyle() { 
   let style = document.createElement("span"); 
-  style.setAttribute("class", "radio"); 
+  style.setAttribute("class", "checkmark"); 
   return style;
 }
 function radioContainer() { 
@@ -16,35 +16,59 @@ function radioContainer() {
   return container;
 }
 
-let startContainer = radioContainer();
+let startContainer = radioContainer("Start/end");
 let start = document.createElement("input");
-let outputContainer = radioContainer();
+let outputContainer = radioContainer("Output");
 let output = document.createElement("input");
-let delayContainer = radioContainer();
+let delayContainer = radioContainer("Delay");
 let delay = document.createElement("input");
-let decisionContainer = radioContainer();
+let decisionContainer = radioContainer("Decision");
 let decision = document.createElement("input");
 
 let separator = document.createElement("hr");
 
 let cursors = document.createElement("div");
 
-let cursorContainer = radioContainer();
+let cursorContainer = radioContainer("Cursor");
 let cursor = document.createElement("input");
-let arrowContainer = radioContainer();
+let arrowContainer = radioContainer("Arrow");
 let arrow = document.createElement("input");
 
+let startIcon = document.createElement("div");
+startIcon.setAttribute("class", "start-icon");
+startIcon.innerText = "Start";
+let outputIcon = document.createElement("div");
+outputIcon.setAttribute("class", "output-icon");
+outputIcon.innerHTML = "<li>Output</li>";
+let delayIcon = document.createElement("div");
+delayIcon.setAttribute("class", "delay-icon");
+delayIcon.innerText = "Delay";
+let decisionIcon = document.createElement("div");
+decisionIcon.setAttribute("class", "decision-icon");
+decisionIcon.innerHTML = "<li>Decision</li>";
+
+let cursorIcon = document.createElement("i");
+cursorIcon.setAttribute("class", "fas fa-mouse-pointer");
+let arrowIcon = document.createElement("i");
+arrowIcon.setAttribute("class", "fas fa-long-arrow-alt-right");
+
+startContainer.appendChild(startIcon);
 startContainer.appendChild(start);
 startContainer.appendChild(radioStyle());
+outputContainer.appendChild(outputIcon);
 outputContainer.appendChild(output);
 outputContainer.appendChild(radioStyle());
+delayContainer.appendChild(delayIcon);
 delayContainer.appendChild(delay);
 delayContainer.appendChild(radioStyle());
+decisionContainer.appendChild(decisionIcon);
 decisionContainer.appendChild(decision);
 decisionContainer.appendChild(radioStyle());
 
+cursorContainer.appendChild(cursorIcon);
 cursorContainer.appendChild(cursor);
 cursorContainer.appendChild(radioStyle());
+arrowContainer.appendChild(arrowIcon);
 arrowContainer.appendChild(arrow);
 arrowContainer.appendChild(radioStyle());
 
@@ -78,3 +102,6 @@ decision.name = "element";
 
 cursor.name = "cursor";
 arrow.name = "cursor";
+
+
+toolbar.style.width = "140px";
