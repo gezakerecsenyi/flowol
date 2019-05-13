f6a05efe3a7a36e3abb0a2422e6c09eb9649d68f
gezakerecsenyi <32415108+gezakerecsenyi@users.noreply.github.com>
2019-05-13 07:52:50 +0100

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,5 +1,6 @@
+let selected, cursor;
+
 Flowol.prototype.buildElem = function(){
-  let selected;
   if (document.forms["elements"].elements["element"][0].checked){
     selected = 0;
   } else if (document.forms["elements"].elements["element"][1].checked){
@@ -12,10 +13,57 @@ Flowol.prototype.buildElem = function(){
 }
 
 Flowol.prototype.switchCursor = function(){
-  let selected;
   if (document.forms["cursors"].elements["cursor"][0].checked){
-    selected = 0;
+    cursor = 0;
   } else {
-    selected = 1;
+    cursor = 1;
+  }
+}
+
+Flowol.prototype.clickHandler = function(e){
+  let workspace = document.getElementById("workspace");
+  console.log("Hi!")
+  if (cursor === 0 && (selected || selected === 0)){
+    let thisShape = document.createElement("div");
+    switch (selected){
+      case 0:
+        thisShape.setAttribute("class", "shape start-shape");
+        break;
+      case 1:
+        thisShape.setAttribute("class", "shape output-shape");
+        break;
+      case 2:
+        thisShape.setAttribute("class", "shape delay-shape");
+        break;
+      case 3:
+        thisShape.setAttribute("class", "shape decision-shape");
+        break;
+    }
+    
+    var mX = e.pageX;
+    var mY = e.pageY;
+    var from = {x:mX, y:mY};
+    var $n=$('#calculator');
+    var off = $n.offset(),
+        nx1 = off.left,
+        ny1 = off.top,
+        nx2 = nx1 + $n.outerWidth(),
+        ny2 = ny1 + $n.outerHeight(),
+        maxX1 = Math.max(mX, nx1),
+        minX2 = Math.min(mX, nx2),
+        maxY1 = Math.max(mY, ny1),
+        minY2 = Math.min(mY, ny2),
+        intersectX = minX2 >= maxX1,
+        intersectY = minY2 >= maxY1,
+        to = {
+            x: intersectX ? mX : nx2 < mX ? nx2 : nx1,
+            y: intersectY ? mY : ny2 < mY ? ny2 : ny1
+        };
+        var distX = Math.abs(to.x - from.x),
+        distY = Math.abs(to.y - from.y);
+
+    thisShape.style.left = distX + "px";
+    thisShape.style.top = distY + "px";
+    workspace.appendChild(thisShape);
   }
 }
